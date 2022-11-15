c755253501eef5949339e1eb6d82fb81c05cc182
Mara Guluzian <mara15.guluzian@gmail.com>
2022-11-15 17:54:47 -0500

    need to fix right versus left click

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,66 @@ export function App() {
   const [game, setGame] = useState<Game>({
     id: null,
     winner: null,
-    state: null,
-    board: [],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    // [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-
+    //state: null,
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
     //let board = Array(8).fill(0).map(row => new Array(8).fill(null))
   })
-
-  type Cell =
-    | ' '
-    | '_'
-    | 'F'
-    | '*'
-    | '@'
-    | '1'
-    | '2'
-    | '3'
-    | '4'
-    | '5'
-    | '6'
-    | '7'
-    | '8'
-  type Row = Array<Cell>
-  type Board = Array<Row>
+  type Square = 'X' | 'O' | ' '
+  type Row = [Square, Square, Square, Square, Square, Square, Square, Square]
+  type Board = [Row, Row, Row, Row, Row, Row, Row, Row]
   type Game = {
     board: Board
-    id: null | number
+    id: number | null
     winner: null | string
-    state: null | string
+  }
+
+  // type Cell =
+  //   | ' '
+  //   | '_'
+  //   | 'F'
+  //   | '*'
+  //   | '@'
+  //   | '1'
+  //   | '2'
+  //   | '3'
+  //   | '4'
+  //   | '5'
+  //   | '6'
+  //   | '7'
+  //   | '8'
+  // type Row = 8cells
+  // type Board = rows
+  // type Game = {
+  //   board: Board
+  //   id: null | number
+  //   winner: null | string
+  //   state: null | string
+  // }
+
+  async function handleNewGame() {
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+      // Make that the new state!
+      setGame(newGame)
+    }
   }
 
   async function handleLeftClickCell(row: number, col: number) {
@@ -69,64 +94,34 @@ export function App() {
     setGame(await response.json())
   }
 
-  async function handleNewGame() {
-    const response = await fetch(
-      'https://minesweeper-api.herokuapp.com/games',
-      {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-      }
-    )
-
-    if (response.ok) {
-      const newGameState = await response.json()
-      setGame(newGameState)
-    }
-
-    let title
-    if (game.state === 'won') {
-      title = 'You Won!'
-    }
-    if (game.state === 'lost') {
-      title = 'You Lost!, maybe next year'
-    }
-    if (game.state === 'new') {
-      title = 'Press a cell to begin'
-    }
-    if (game.state === 'playing') {
-      title = 'Playing, press new game to start over'
-    }
-
-    return (
-      <div>
-        <h1 className="glitch">MINESWEEPER</h1>
-        <div className="difficulties">
-          <button className="difficult">Difficult</button>
-          <button className="medium">Medium</button>
-          <button className="easy">Easy</button>
-        </div>
-        <ul>
-          {game.board.map((row, rowIndex) =>
-            row.map((cell, columnIndex) => (
-              <li
-                key={columnIndex}
-                onClick={() => handleLeftClickCell(rowIndex, columnIndex)}
-              >
-                {cell}
-              </li>
-            ))
-          )}
-        </ul>
-        <footer>
-          <button
-            onClick={handleNewGame}
-            className="glow-on-hover"
-            type="button"
-          >
-            NEW GAME
-          </button>
-        </footer>
+  return (
+    <div>
+      <h1 className="glitch">MINESWEEPER</h1>
+      <div className="difficulties">
+        <button className="difficult">Difficult</button>
+        <button className="medium">Medium</button>
+        <button className="easy">Easy</button>
       </div>
-    )
-  }
+
+      <ul>
+        {game.board.map((row, rowIndex) =>
+          row.map((column, columnIndex) => (
+            <li
+              onClick={() => {
+                handleLeftClickCell(rowIndex, columnIndex)
+              }}
+              key={column}
+            >
+              {column}
+            </li>
+          ))
+        )}
+      </ul>
+      <footer>
+        <button onClick={handleNewGame} className="glow-on-hover" type="button">
+          NEW GAME
+        </button>
+      </footer>
+    </div>
+  )
 }
