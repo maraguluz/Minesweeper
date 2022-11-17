4d44bbdb118944baef290a8f392428837e5cb4c8
Mara Guluzian <mara15.guluzian@gmail.com>
2022-11-17 13:21:51 -0500

    switch statement for styling the cells

--- src/App.tsx
+++ src/App.tsx
@@ -4,64 +4,43 @@ export function App() {
   const [game, setGame] = useState<Game>({
     id: null,
     winner: null,
-    //state: null,
-    board: [
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ],
-    //let board = Array(8).fill(0).map(row => new Array(8).fill(null))
+    board: [],
   })
-  type Square = 'X' | 'O' | ' '
-  type Row = [Square, Square, Square, Square, Square, Square, Square, Square]
-  type Board = [Row, Row, Row, Row, Row, Row, Row, Row]
+  type Cell =
+    | ' '
+    | '_'
+    | 'F'
+    | '*'
+    | '@'
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+  type Row = Array<Cell>
+  type Board = Array<Row>
   type Game = {
     board: Board
     id: number | null
     winner: null | string
   }
 
-  // type Cell =
-  //   | ' '
-  //   | '_'
-  //   | 'F'
-  //   | '*'
-  //   | '@'
-  //   | '1'
-  //   | '2'
-  //   | '3'
-  //   | '4'
-  //   | '5'
-  //   | '6'
-  //   | '7'
-  //   | '8'
-  // type Row = 8cells
-  // type Board = rows
-  // type Game = {
-  //   board: Board
-  //   id: null | number
-  //   winner: null | string
-  //   state: null | string
-  // }
-
-  async function handleNewGame() {
+  async function handleNewGame(difficulty: number) {
     const response = await fetch(
-      'https://minesweeper-api.herokuapp.com/games',
+      `https://minesweeper-api.herokuapp.com/games?difficulty=${difficulty}}`,
       {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ difficulty }),
       }
     )
 
     if (response.ok) {
-      // Get the response as JSON
       const newGame = await response.json()
-      // Make that the new state!
+
       setGame(newGame)
     }
   }
@@ -94,23 +73,50 @@ export function App() {
     setGame(await response.json())
   }
 
+  function transformCellClassName(value: string | number) {
+    switch (value) {
+      case 'F':
+        return 'cell-flag'
+
+      case '*':
+        return 'cell-bomb'
+
+      case '_':
+        return 'cell-free'
+    }
+    if ([1, 2, 3, 4, 5, 6, 7, 8].includes(Number(value))) {
+      return `cell-number cell-${value}`
+    }
+    return undefined
+  }
   return (
     <div>
       <h1 className="glitch">MINESWEEPER</h1>
+
       <div className="difficulties">
-        <button className="difficult">Difficult</button>
-        <button className="medium">Medium</button>
-        <button className="easy">Easy</button>
+        <button onClick={() => handleNewGame(0)} className="easy">
+          EASY
+        </button>
+        <button onClick={() => handleNewGame(1)} className="medium">
+          MEDIUM
+        </button>
+        <button onClick={() => handleNewGame(2)} className="difficult">
+          DIFFICULT
+        </button>
       </div>
 
-      <ul>
+      <ul className={`difficulty-${game.board.length}`}>
         {game.board.map((row, rowIndex) =>
           row.map((column, columnIndex) => (
             <li
               onClick={() => {
                 handleLeftClickCell(rowIndex, columnIndex)
               }}
-              key={column}
+              onContextMenu={(event) => {
+                event.preventDefault()
+                handleRightClickCell(rowIndex, columnIndex)
+              }}
+              key={`${rowIndex}-${columnIndex}`}
             >
               {column}
             </li>
@@ -118,7 +124,11 @@ export function App() {
         )}
       </ul>
       <footer>
-        <button onClick={handleNewGame} className="glow-on-hover" type="button">
+        <button
+          onClick={() => handleNewGame(0 ? 1 : 2)}
+          className="glow-on-hover"
+          type="button"
+        >
           NEW GAME
         </button>
       </footer>
---
+++ src/components/Sounds.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+// sound = new Audio('https://www.soundjay.com/button/sounds/beep-07.mp3')
+// sound = 'C:Usersmara1Downloadsmixkit-bomb-drop-impact-2804.wav'
